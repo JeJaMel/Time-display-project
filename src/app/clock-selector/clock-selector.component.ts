b7f7b82a49d5adb9ee70802de2fa2f6f1f9c6e2f
JeJaMel <jjmelendezb15@gmail.com>
2025-03-20 20:25:55 -0400

    feat: Initialize components

---
+++ src/app/clock-selector/clock-selector.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clock-selector',
+  imports: [],
+  templateUrl: './clock-selector.component.html',
+  styleUrl: './clock-selector.component.css'
+})
+export class ClockSelectorComponent {
+
+}
---
+++ src/app/time-slider/time-slider.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-time-slider',
+  imports: [],
+  templateUrl: './time-slider.component.html',
+  styleUrl: './time-slider.component.css'
+})
+export class TimeSliderComponent {
+
+}
